d3ed7f5e678248847a964f8389306c24d1e6a22f
Bennett Clement <benclmnt.dev@gmail.com>
2020-07-06 00:43:14 +0800

    Finish implementing email lambdas

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,25 +1,59 @@
-require('dotenv').config();
-const fetch = require('node-fetch');
-const { EMAIL_TOKEN } = process.env;
+const sgMail = require('@sendgrid/mail');
+const { SENDGRID_API_KEY, ANGKLUNG_EMAIL } = process.env;
 
-exports.handler = function (event, context, callback) {
-    const email = JSON.parse(event.body).payload.email;
-    console.log(`Received a submission: ${email}`);
-    return {
-        statusCode: 200,
-        body: JSON.stringify(email),
+// setup sendgrid
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+exports.handler = async function (event, context, callback) {
+    const { name, email, message, formType } = JSON.parse(
+        event.body
+    ).payload.data;
+
+    // set up email msg. We are sending from our self to ourself.
+    const responseSubject = encodeURIComponent(
+        `Regarding your Angklung Ensemble enquiry, ${name}`
+    );
+    const msg = {
+        to: ANGKLUNG_EMAIL,
+        from: ANGKLUNG_EMAIL,
+        subject: `${formType} - ${name}`,
+        text: message,
+        html: `
+        <div style="padding: 10px; font-size: 16px;">
+            <h2>Hola, admins!</h2>
+            <p>
+                We've got a new enquiry here! To reply directly, click the button below!
+            </p>
+            <p>Enquiry Details: </p>
+            <ul>
+                <li>Name: ${name}</li>
+                <li>Email: ${email}</li>
+                <li>Message: ${message}</li>
+            </ul>
+            <a
+                href="mailto:${email}?subject=${responseSubject}"
+                style="text-decoration: none; text-align: center;">
+                <div
+                    style="height: 24px; width: 80px; color: white; padding: 10px;
+                    border-radius: 5px; background-color: blue;">
+                    Reply now!
+                </div>
+            </a>
+        </div>
+        `,
     };
-    // return fetch('https://api.buttondown.email/v1/subscribers', {
-    //     method: 'POST',
-    //     headers: {
-    //         Authorization: `Token ${EMAIL_TOKEN}`,
-    //         'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ email }),
-    // })
-    //     .then((response) => response.json())
-    //     .then((data) => {
-    //         console.log(`Submitted to Buttondown:\n ${data}`);
-    //     })
-    //     .catch((error) => ({ statusCode: 422, body: String(error) }));
+    console.log(msg);
+    // now send the email
+    try {
+        // const res = await sgMail.send(msg);
+        return {
+            statusCode: 200,
+            body: JSON.stringify('success'),
+        };
+    } catch (error) {
+        return {
+            statusCode: 422,
+            body: JSON.stringify(error),
+        };
+    }
 };
