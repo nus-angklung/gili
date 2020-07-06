ecca08a47a56f3ccab65007c679af74be001dead
Bennett Clement <benclmnt.dev@gmail.com>
2020-07-06 19:52:35 +0800

    Add our music page

--- src/routes/our-music/index.svelte
+++ src/routes/our-music/index.svelte
@@ -3,7 +3,40 @@
 </script>
 
 <style>
+    .iframe-container {
+        overflow: hidden;
+        position: relative;
+    }
 
+    .iframe-container iframe {
+        border: 0;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    .iframe-container-16x9 {
+        padding-bottom: calc(9 / 16 * 100%);
+    }
+
+    .caption {
+        text-align: center;
+        padding-top: 2em;
+    }
 </style>
 
-<h1>Our Music</h1>
+<!-- <h4>Our Music</h4> -->
+
+<div class="iframe-container iframe-container-16x9">
+    <iframe
+        title="Angklung latest stage performance"
+        src="https://www.youtube.com/embed/8JgN5XLcVa8?rel=0"
+        class=""
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope;
+        picture-in-picture"
+        allowfullscreen />
+</div>
+<div class="caption">Angklung Performance (2019)</div>
