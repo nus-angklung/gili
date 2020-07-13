61b23bebafdd59f49e8dff400954114f402d29c5
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-07-13 14:57:19 +0800

    Add angklung images and basic display

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,24 @@
 <style>
-
+    .navigation-button {
+        border: solid 2px white;
+        padding: 10px;
+        margin: 0 4px 4px 4px;
+        border-radius: 10px;
+        background-color: black;
+        color: white;
+    }
 </style>
 
 <svelte:head>
     <title>NUS Angklung Ensemble</title>
 </svelte:head>
 
-<h1>Welcome</h1>
+<img src="/homepage/header.svg" width="50%" />
+<br />
+<p>Listen to the sound of Nusantara</p>
+<button class="navigation-button">Join Us Now</button>
+<button class="navigation-button">Learn To Play</button>
+<br />
+<img src="/homepage/angklung-small.svg" height="200" />
+<img src="/homepage/angklung-mid.svg" height="300" />
+<img src="/homepage/angklung-large.svg" height="400" />
