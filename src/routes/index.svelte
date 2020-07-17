b59765399855267875401253452e9143e25fa0cc
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-07-18 00:37:17 +0800

    Chagne to grid way

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -55,15 +55,15 @@
         margin: auto;
         position: relative;
 
-        display: flex;
+        display: grid;
+        grid-template-columns: auto auto;
+        grid-gap: 20px;
+
         justify-content: center;
-        align-items: center;
-        flex-flow: row wrap;
     }
 
     .card {
         width: 300px;
-        margin: 20px;
         padding: 25px;
         background-color: rgba(250, 250, 250, 0.2);
     }
