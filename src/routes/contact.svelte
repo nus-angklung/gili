6d5c328038968bc2ab05408c131534593fb06c27
Bennett Clement <benclmnt.dev@gmail.com>
2020-07-18 15:19:15 +0800

    Update contact us page -- skeleton :)

---
+++ src/routes/contact.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+    <title>Contact us</title>
+</svelte:head>
+<h1>Find us at our social media!</h1>
+
+<ul>
+    <li>
+        <a href="https://www.instagram.com/nusangklung">Instagram</a>
+    </li>
+    <li>
+        <a href="https://www.facebook.com/NUSAngklung">Facebook</a>
+    </li>
+    <li>
+        <a href="https://www.youtube.com/channel/UC86Z1UcM82LOKvlS2HyQZSg">
+            Youtube
+        </a>
+    </li>
+</ul>
