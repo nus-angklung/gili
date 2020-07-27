70834715ff4f6830c8ebc4030b938dbbe39a144a
Yehezkiel Raymundo Theodoroes <39835686+Yehezkiel01@users.noreply.github.com>
2020-07-27 11:49:08 +0800

    Styling contact us page (#14)
    * Styling contact us page
    
    * Fix styling
    
    * Set width as class property

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,18 +1,45 @@
+<style>
+    .social {
+        display: flex;
+        justify-content: center;
+        flex-flow: row wrap;
+    }
+
+    .social a {
+        padding: 1rem;
+    }
+
+    .social a img {
+        width: 100px;
+    }
+</style>
+
 <svelte:head>
     <title>Contact us</title>
 </svelte:head>
-<h1>Find us at our social media!</h1>
+<center>
+    <h1>Find us at our social media!</h1>
+</center>
 
-<ul>
-    <li>
-        <a href="https://www.instagram.com/nusangklung">Instagram</a>
-    </li>
-    <li>
-        <a href="https://www.facebook.com/NUSAngklung">Facebook</a>
-    </li>
-    <li>
-        <a href="https://www.youtube.com/channel/UC86Z1UcM82LOKvlS2HyQZSg">
-            Youtube
-        </a>
-    </li>
-</ul>
+<div class="social">
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.facebook.com/NUSAngklung">
+        <img src="client/contact/facebook.svg" />
+    </a>
+
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.youtube.com/channel/UC86Z1UcM82LOKvlS2HyQZSg">
+        <img src="client/contact/youtube.svg" />
+    </a>
+
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.instagram.com/nusangklung">
+        <img src="client/contact/instagram.svg" />
+    </a>
+</div>
