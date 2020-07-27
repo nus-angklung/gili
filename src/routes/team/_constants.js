b249f5147ad8001bec9a3a485fcdb32ec8996d94
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-07-27 11:01:04 +0800

    Add Readme

--- src/routes/team/_constants.js
+++ src/routes/team/_constants.js
@@ -1,3 +1,6 @@
 /* This file contains constants value specific to the team pages */
-export const current_year = 2020;
+
 export const first_year = 2007;
+
+// Reminder to keep the current_year value updated every year
+export const current_year = 2020;
