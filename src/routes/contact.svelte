9f55dd23aa88c937da6e5a4e15e129c0894896ec
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2020-07-27 10:39:34 +0800

    Styling contact us page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,18 +1,41 @@
+<style>
+    .social {
+        display: flex;
+        justify-content: center;
+        margin: 3rem 10rem;
+    }
+
+    .social a {
+        margin: 0 auto;
+    }
+</style>
+
 <svelte:head>
     <title>Contact us</title>
 </svelte:head>
-<h1>Find us at our social media!</h1>
+<center>
+    <h1>Find us at our social media!</h1>
+</center>
 
-<ul>
-    <li>
-        <a href="https://www.instagram.com/nusangklung">Instagram</a>
-    </li>
-    <li>
-        <a href="https://www.facebook.com/NUSAngklung">Facebook</a>
-    </li>
-    <li>
-        <a href="https://www.youtube.com/channel/UC86Z1UcM82LOKvlS2HyQZSg">
-            Youtube
-        </a>
-    </li>
-</ul>
+<div class="social">
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.facebook.com/NUSAngklung">
+        <img src="client/contact/facebook.svg" width="100px" />
+    </a>
+
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.youtube.com/channel/UC86Z1UcM82LOKvlS2HyQZSg">
+        <img src="client/contact/youtube.svg" width="100px" />
+    </a>
+
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.instagram.com/nusangklung">
+        <img src="client/contact/instagram.svg" width="100px" />
+    </a>
+</div>
