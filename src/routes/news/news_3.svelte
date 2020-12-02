d30707b6f73e6bd8f3a1c06b7a9cdc8c3a56525b
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2020-12-02 18:26:50 +0800

    Add 'news' folder to the src/routes/

---
+++ src/routes/news/news_3.svelte
@@ -0,0 +1,208 @@
+<html lang="en">
+<head> 
+	<meta charset="utf-8"> 
+	<meta content="width=device-width,initial-scale=1" name="viewport">
+	<meta content="#333333" name="theme-color"> 
+	<base href="/"> 
+	<link href="normalize.css" rel="stylesheet"> 
+	<link href="global.css" rel="stylesheet">
+	<link href="manifest.json" rel="manifest" crossorigin="use-credentials"> 
+	<link href="favicon.jpg" rel="icon" type="image/jpg">
+	<link href="client/main.1645456778.css" rel="stylesheet"> 
+	<script src="/client/client.aa67dc9d.js" type="module" crossorigin="use-credentials">
+	</script>
+	<script src="jquery.js"></script> 
+    	<script> 
+    		$(function(){
+    			$("#includedContent").load(".html"); 
+    		});
+    	</script>
+ 
+	<title>
+		CCA News
+	</title>
+</head>
+<body> 
+	<div id="sapper">
+		<nav class="container svelte-15h10pp">
+			<ul class="svelte-15h10pp">
+				<li class="svelte-15h10pp">
+					<a class="svelte-15h10pp" href="." aria-current="page">Home</a>
+				</li>
+				<li class="dropdown svelte-15h10pp">
+					<ul class="dropdown-content svelte-15h10pp">
+						<li class="svelte-15h10pp">
+							<a class="svelte-15h10pp" href="about">
+								About Us
+							</a> 
+						</li>
+						<li class="svelte-15h10pp">
+							<a class="svelte-15h10pp" href="team">
+								Team
+							</a>
+						 </li>
+						<li class="svelte-15h10pp">
+							<a class="svelte-15h10pp" href="CCA_News">
+								CCA News
+							</a> 
+						</li>
+					</ul>
+				<span class="svelte-15h10pp">Our Ensemble</span>
+				</li>
+				<li class="svelte-15h10pp">
+					<a class="svelte-15h10pp" href="our-music">
+						Our Music
+					</a>
+				</li>
+				<li class="svelte-15h10pp">
+					<a class="svelte-15h10pp" href="contact">
+						Contact Us
+					</a>
+				</li>
+			</ul>
+		</nav> 
+		<main class="svelte-1elnl9h">
+			<p>
+				###########
+			</p>
+			<br class="svelte-u1musi">
+			<img alt="ABCABC" class="svelte-u1musi", "center" src="client/News/kolintang.jpg">
+			<p>
+				Date: dd/mm/yyyy
+			</p>
+			<br class="svelte-u1musi">
+			<p>
+				######################################
+				######################################
+				######################################
+				######################################
+
+				######################################
+				######################################
+				######################################
+				######################################
+				
+				######################################
+				######################################
+				######################################
+				######################################
+			
+				######################################
+				######################################
+				######################################
+				######################################
+			</p>
+			<br class="svelte-u1musi">
+			<div class="navigation svelte-15f4hwq">
+				<a class="navigation-button svelte-15f4hwq" href="CCA_NEWS/2">
+					Previous
+				</a> 
+			</div>
+		</main> 
+		<footer class="svelte-1yh7l9">
+			<div class="form container svelte-1yh7l9">
+				<h6 class="question svelte-1yh7l9">
+					Have a passion for Angklung? Have an idea for collaboration?
+				</h6> 
+				<h6 class="answer svelte-1yh7l9">
+					Contact us! We are happy to hear from you!
+				</h6> 
+				<div class="svelte-1yh7l9">
+					<form action="/" class="contact-form svelte-1mkyfau" method="post" name="contact" netlify-honeypot="bot-field" data-netlify="true">
+						<input type="hidden" name="bot-field" class="svelte-1mkyfau"> 
+						<input class="svelte-1mkyfau" name="formType" type="hidden" value="Web Enquiry"> 
+						<div class="first-row svelte-1mkyfau">
+							<div class="name-col svelte-1mkyfau">
+								<label class="svelte-1mkyfau" for="contact__name">
+									First Name
+								</label> 
+								<input class="svelte-1mkyfau" name="name" id="contact__name" required="" type="text"> 
+								<div class="text-input-border svelte-1mkyfau">
+								</div>
+							</div> 
+							<div class="spacer svelte-1mkyfau">
+							</div> 
+							<div class="email-col svelte-1mkyfau">
+								<label class="svelte-1mkyfau" for="contact__email">
+									Email
+								</label> 
+								<input class="svelte-1mkyfau" name="email" type="email" id="contact__email" required=""> 
+								<div class="text-input-border svelte-1mkyfau">
+								</div>
+							</div>
+						</div> 
+						<label class="svelte-1mkyfau" for="contact__message">
+							Message:
+						</label> 
+						<textarea class="svelte-1mkyfau" id="contact__message" name="message" required="">
+						</textarea> 
+						<div class="last-row svelte-1mkyfau">
+							<button class="button-clear svelte-1mkyfau hidden">
+								Cancel
+							</button> 
+							<button class="svelte-1mkyfau" type="submit">
+								Submit
+							</button>
+						</div>
+					</form>
+				</div>
+			</div> 
+			<div class="links svelte-1yh7l9">
+				<div class="container links-container svelte-1yh7l9">
+					<div class="links-col svelte-1yh7l9">
+						<div class="svelte-1yh7l9">
+							Links
+						</div> 
+						<ul class="svelte-1yh7l9">
+							<li class="svelte-1yh7l9">
+								<a class="svelte-1yh7l9" href=".">
+									Home
+								</a>
+							</li> 
+							<li class="svelte-1yh7l9">
+								<a class="svelte-1yh7l9" href="/about">
+									About us
+								</a>
+							</li> 
+							<li class="svelte-1yh7l9">
+								<a class="svelte-1yh7l9" href="/our-music">
+									Our Music
+								</a>
+							</li>
+						</ul>
+					</div> 
+					<div class="links-col svelte-1yh7l9">
+						<div class="svelte-1yh7l9">
+							Social
+						</div> 
+						<ul class="svelte-1yh7l9">
+							<li class="svelte-1yh7l9">
+								<a class="svelte-1yh7l9" href="https://www.instagram.com/nusangklung">
+									Instagram
+								</a>
+							</li> 
+							<li class="svelte-1yh7l9">
+								<a class="svelte-1yh7l9" href="https://www.facebook.com/NUSAngklung">
+									Facebook
+								</a>
+							</li> 
+							<li class="svelte-1yh7l9">
+								<a class="svelte-1yh7l9" href="https://www.youtube.com/channel/UC86Z1UcM82LOKvlS2HyQZSg">
+									Youtube
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div> 
+			<div class="copyright-tag svelte-1yh7l9">
+				Copyright 2020 - Present. NUS Angklung Ensemble. Sub-club of NUS
+        				Cultural Activities Club.
+			</div>
+		</footer>
+	</div> 
+	<script>
+		__SAPPER__={baseUrl:"",preloaded:[void 0,{}]};if('serviceWorker' in navigator)navigator.serviceWorker.register('/service-worker.js');(function(){try{eval("async function x(){}");var main="/client/client.aa67dc9d.js"}catch(e){main="/client/legacy/client.f8725534.js"};var s=document.createElement("script");try{new Function("if(0)import('')")();s.src=main;s.type="module";s.crossOrigin="use-credentials";}catch(e){s.src="/client/shimport@1.0.1.js";s.setAttribute("data-main",main);}document.head.appendChild(s);}());
+	</script> 
+</body>
+</html>
