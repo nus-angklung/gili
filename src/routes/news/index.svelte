4a3bd56033efbb52c027f047bee5b41c2de67396
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2020-12-07 15:07:16 +0800

    Update index.svelte

--- src/routes/news/index.svelte
+++ src/routes/news/index.svelte
@@ -1,6 +1,6 @@
 <script context="module">
-    import { current_date } from './_constants.js';
+    import { current_code } from './_constants.js';
     export function preload({ params, query }) {
-        return this.redirect(302, `new/` + current_date);
+        return this.redirect(302, `news`);
     }
 </script>
