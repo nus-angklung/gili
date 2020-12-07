27135fbbb166ddc7a6fe1de66fed88f84f9982c2
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2020-12-07 16:53:03 +0800

    Delete index.svelte

--- src/routes/news/index.svelte
+++
@@ -1,6 +0,0 @@
-<script context="module">
-    import { current_code } from './_constants.js';
-    export function preload({ params, query }) {
-        return this.redirect(302, `news`);
-    }
-</script>
