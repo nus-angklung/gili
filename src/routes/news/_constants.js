58f2cf5dc0b5e18425531b0384016f9011d3278d
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2020-12-07 16:52:34 +0800

    Delete _constants.js

--- src/routes/news/_constants.js
+++
@@ -1,6 +0,0 @@
-/* This file contains constants value specific to the team pages */
-
-export const first_code = 1;
-
-// Reminder to keep the current_year value updated every year
-export const current_code = 3;
