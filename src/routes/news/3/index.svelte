0babd51e6ed6a6d46e714b3056bb3592527c7785
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2020-12-07 17:19:20 +0800

    Update index.svelte

--- src/routes/news/3/index.svelte
+++ src/routes/news/3/index.svelte
@@ -4,39 +4,10 @@
     }
     h2 {
         color: #f5eccf;
-        position: center;
-    }
-    h2:not(:first-child) {
-        margin-top: 3rem;
-    }
-    .grid {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        column-gap: 2rem;
-        row-gap: 2rem;
-    }
-    .photo {
-        padding: 1rem 0;
-        margin: auto;
-        grid-column: 1 / 2;
-    }
-    .photo--text {
-        position: relative;
-        grid-column: 2 / 4;
+        text-align: center;
     }
     .banner {
         width: 100%;
-    }
-    .photo--text h3 {
-        line-height: 56px;
-    }
-    .photo--text h3::after {
-        position: absolute;
-        content: '';
-        width: calc(100% - 1em);
-        height: 2px;
-        background-color: var(--highlight);
-        display: block;
     }
     .navigation {
         display: flex;
@@ -54,37 +25,12 @@
     p {
         opacity: 0.8;
     }
-    .photo img {
-        width: 100%;
-    }
-    a:hover {
-        color: tomato;
-    }
     @media (max-width: 992px) {
         .photo,
         .photo--text {
             grid-column: 1 / -1;
         }
     }
-    /* .photo .hover {
-        position: absolute;
-        bottom: 10%;
-    }
-    .photo .hover::after {
-        content: '';
-        background-color: white;
-        opacity: 0.7;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-    }
-    .photo:hover .hover {
-        opacity: 1;
-    }
-    .photo--caption {
-        text-align: center;
-        width: 100%;
-    } */
 </style>
 
 <svelte:head>
@@ -92,6 +38,7 @@
 </svelte:head>
 
 <h2>ABC</h2>
+<br>
 <img
     src="client/news/3/angklung.jpg"
     class="banner"
@@ -99,7 +46,6 @@
 <p>
     Date: 13 November 2020
 </p>
-<br>
 <br>
 <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis neque ac ante lacinia vestibulum. Suspendisse ligula tortor, egestas ac tincidunt nec, finibus vel orci. Maecenas gravida commodo felis ut tincidunt. 
