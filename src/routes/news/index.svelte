07cefc22b35ba0dd54f375ca41f67d214134dae3
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2021-01-06 15:47:21 +0800

    Created index.svelte for news

---
+++ src/routes/news/index.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+    import { current_number } from './constants.js';
+    import news from './news.js';
+    const dates = Object.keys(news);
+    export function preload({ params, query }) {
+        return this.redirect(302, `news/` + dates[current_number]);
+    }
+</script>
