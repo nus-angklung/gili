cbe0ba258ccb26b8fbac3c095046c3a15f56f072
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2021-01-06 15:46:28 +0800

    Created the constants.js in the news

---
+++ src/routes/news/constants.js
@@ -0,0 +1,9 @@
+export const first_number = 0;
+
+// Reminder to keep the current_year value updated every year
+
+import news from './news.js';
+
+const dates = Object.keys(news);
+
+export const current_number = dates.length - 1;
