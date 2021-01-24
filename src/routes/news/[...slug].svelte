c0932cf6edbe78466672533f26d8f58779f7057e
kevinchs0808 <62506934+kevinchs0808@users.noreply.github.com>
2021-01-24 11:53:48 +0800

    Deleted unnecessary spaces and added some comments

--- src/routes/news/[...slug].svelte
+++ src/routes/news/[...slug].svelte
@@ -1,22 +1,20 @@
 <script context="module">
     import newsTargets from 'news-targets';
-
     export async function preload({ params, query }) {
         // the `slug` parameter is available because
         // this file is called [slug].svelte
         const [slug, title] = params.slug;
         const res = await this.fetch(`news/${slug}.json`);
         const data = await res.json();
-
         // if title exists, try to find news with matching title. if not found, get the first one
         // TODO: current support for multiple articles with same date is still hacky
         let news =
             (title && data.news.find((news) => news.title == title)) ||
             data.news[0];
-
         // https://github.com/rollup/rollup/issues/2463#issuecomment-455957865
         news.file = await newsTargets[news.file]().then((x) => x.default);
-
+        // This if-else statement is used to make sure that the web page can be displayed (redirect status of 200). 
+        // Otherwise, an error message will be displayed.
         if (res.status === 200) {
             return {
                 news,
@@ -27,31 +25,28 @@
         }
     }
 </script>
-
 <script>
     import { dates } from './_news.js';
     export let number;
     export let news = {};
     const default_picture = 'client/news/default-picture.svg';
-
     // convert image slug (if any) into image source or return a default source.
     function get_image_source(image_slug) {
         if (!image_slug) {
             return default_picture;
         }
-
         return 'client/news/' + dates[number] + '/' + image_slug;
     }
 </script>
-
 <style>
+    // This is used for positioning the buttons in the web page.
     .navigation {
         display: flex;
         justify-content: center;
         align-items: baseline;
         flex-flow: row wrap;
     }
-
+    // This is used to provide the design of the buttons.
     .navigation-button {
         border: solid 1px white;
         padding: 0.5rem 1rem;
@@ -59,7 +54,7 @@
         border-radius: 5px;
         text-decoration: none;
     }
-
+    // This is used to make sure that the image is centered in the web page.
     .center_image {
         display: block;
         margin-left: auto;
@@ -67,11 +62,9 @@
         width: 100%;
     }
 </style>
-
 <svelte:head>
     <title>{news.title}</title>
 </svelte:head>
-
 <h1 style="text-align: center">{news.title}</h1>
 <br />
 <img
@@ -80,11 +73,8 @@
     class="center_image" />
 <p>Date: {news.displayDate}</p>
 <br />
-
 <p>{news.file}</p>
-
 <br />
-
 <div class="navigation">
     {#if Number(number) > 0}
         <a class="navigation-button" href="news/{dates[Number(number) - 1]}">
