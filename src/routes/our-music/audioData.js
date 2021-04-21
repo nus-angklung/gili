edffe182ce8e91b41673fab80c9b2db83d2cfdeb
Yehezkiel01 <yehezkieltheodoroes@yahoo.co.id>
2021-04-21 23:46:49 +0800

    Add es lilin

--- src/routes/our-music/audioData.js
+++ src/routes/our-music/audioData.js
@@ -24,4 +24,10 @@ export const audioData = [
             'https://www.dropbox.com/s/htfti89dn6yh491/Viva%20la%20Vida%20-%20Coldplay%20%28Cover%20by%20NUSAE%202014%29.mp3?dl=1',
         name: 'Viva la Vida - Coldplay (Cover by NUSAE 2014)',
     },
+    {
+        link:
+            'https://www.dropbox.com/s/e1xrvw4susfysio/Es%20Lilin%20-%20Sundanese%20Traditional%20Song%20by%20Nining%20Meida%20%28Cover%20by%20NUSAE%202014%29.mp3?dl=1',
+        name:
+            'Es Lilin - Sundanese Traditional Song by Nining Meida (Cover by NUSAE 2014)',
+    },
 ];
