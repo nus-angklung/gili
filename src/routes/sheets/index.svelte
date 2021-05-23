25b468e921f6b82147b0b62ca70b0fd4ced5552f
Bennett Clement <ben@benclmnt.com>
2021-05-23 20:31:54 +0800

    Increase css specificity to override vendor css

--- src/routes/sheets/index.svelte
+++ src/routes/sheets/index.svelte
@@ -18,41 +18,41 @@
 <div style="height: 12rem"/>
 
 <style>
-    :global(div[data-svelte-typeahead]), :global([data-svelte-typeahead] ul) {
+    :global(div[data-svelte-typeahead]), :global(div[data-svelte-typeahead] ul) {
         box-shadow: 0px 2px 16px 2px rgba(251, 191, 36, 1);
     }
 
     /* The dropdown */
-    :global([data-svelte-typeahead] ul) {
+    :global(div[data-svelte-typeahead] ul) {
         margin-top: 5px;
         flex-direction: column;
         z-index: 1;
     }
 
-    :global([data-svelte-typeahead] li) {
+    :global(div[data-svelte-typeahead] li) {
         background-color: black;
         border: none;
         padding: 0.875rem;
     }
 
-    :global([data-svelte-typeahead] li:last-child) {
+    :global(div[data-svelte-typeahead] li:last-child) {
         border-radius: 0 0 .375rem .375rem;
     }
 
-    :global([data-svelte-typeahead] li.selected),
-    :global([data-svelte-typeahead] li:hover),
-    :global([data-svelte-typeahead] li.selected:hover) {
+    :global(div[data-svelte-typeahead] li.selected),
+    :global(div[data-svelte-typeahead] li:hover),
+    :global(div[data-svelte-typeahead] li.selected:hover) {
         background-color: rgba(31, 41, 55, 1); 
     }
 
     /* highlight letters matched */
-    :global([data-svelte-typeahead] mark) {
+    :global(div[data-svelte-typeahead] mark) {
         color: rgba(251, 191, 36, 1);
         background-color: inherit;
     }
 
     /* the search box */
-    :global([data-svelte-search] input) {
+    :global(form[data-svelte-search] input) {
         background-color: black;
         border-radius: 0.375rem;
         font-size: 1.5rem;
