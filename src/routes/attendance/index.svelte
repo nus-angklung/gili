b4a8b3e5104812c6ca816272cf123eedb9782b80
Francisco Chensan <chensanfrancisco@gmail.com>
2021-07-18 22:10:48 +0800

    Migrate attendance system  (#69)
    * Add generate unique QR code feature
    
    * Add functions to get and update unique code from Notion
    
    * Add check-in functionality + update redirect function
    
    * Fix bugs in markAttendance function
    
    * Detect invalid nusnet
    
    * Tidy up error message
    
    * Fix qr code url bug
    
    * Update netlify.toml
    
    * Update README.md
    
    * Add template files
    
    * Add layout reset for attendance pages
    
    * Migrate generate QR functionality
    
    * Migrate check in functionality
    
    * Add password verification before generating QR
    
    * Format some files
    
    * Minor fix for qr code generation function
    
    * Change /js/ -> /api/
    
    * Minor fixes
    
    * Fix netlify.toml to notice functions
    
    * Refactor to using api instead of netlify function
    
    * Fix update attendance flow
    
    * Add logging
    
    Co-authored-by: Bennett Clement <ben@benclmnt.com>
    Co-authored-by: benclmnt <49342399+benclmnt@users.noreply.github.com>

---
+++ src/routes/attendance/index.svelte
@@ -0,0 +1,92 @@
+<script>
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        if (window.localStorage.getItem('name')) {
+            document.querySelector('#name').value =
+                window.localStorage.getItem('name');
+            document.querySelector('#nusnet').value =
+                window.localStorage.getItem('nusnet');
+        }
+
+        document.querySelector('#form').addEventListener('submit', (event) => {
+            window.localStorage.setItem(
+                'name',
+                document.querySelector('#name').value
+            );
+            window.localStorage.setItem(
+                'nusnet',
+                document.querySelector('#nusnet').value
+            );
+        });
+    });
+
+    async function submitHandler() {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get('code');
+        const name = document.querySelector('#name').value;
+        const nusnet = document.querySelector('#nusnet').value;
+
+        const url = new URL('/api/update-attendance', window.location.origin);
+        url.searchParams.append('code', code);
+        url.searchParams.append('name', name);
+        url.searchParams.append('nusnet', nusnet);
+
+        await fetch(url).then((response) => {
+            if (response.status == 200) {
+                response
+                    .text()
+                    .then((location) => window.location.replace(location));
+            } else {
+                response.text().then((message) => alert(message));
+            }
+        });
+    }
+</script>
+
+<div
+    class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+>
+    <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Angklung Check-In
+        </h2>
+        <div>
+            <form
+                on:submit|preventDefault={submitHandler}
+                id="form"
+                class="mt-8 space-y-6"
+            >
+                <div>
+                    <label class="sr-only" for="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        placeholder="Full Name"
+                        required
+                        class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    />
+                </div>
+                <div>
+                    <label class="sr-only" for="nusnet">NUSNET</label>
+                    <input
+                        type="text"
+                        id="nusnet"
+                        name="nusnet"
+                        placeholder="NUSNET id"
+                        required
+                        class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    />
+                </div>
+                <div>
+                    <button
+                        class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Check in
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
