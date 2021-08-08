a2664a9f8645d21c73f765367191f24cd2c11861
fchensan <chensanfrancisco@gmail.com>
2021-08-08 13:44:36 +0800

    Add loading animation mechanism

--- src/routes/attendance/index.svelte
+++ src/routes/attendance/index.svelte
@@ -1,6 +1,9 @@
 <script>
     import { onMount } from 'svelte';
 
+    let buttonInitialHTML;
+    let checkInButton;
+
     onMount(() => {
         if (window.localStorage.getItem('name')) {
             document.querySelector('#name').value =
@@ -22,6 +25,7 @@
     });
 
     async function submitHandler() {
+        startLoadingAnimation();
         const urlParams = new URLSearchParams(window.location.search);
         const code = urlParams.get('code');
         const name = document.querySelector('#name').value;
@@ -39,8 +43,31 @@
                     .then((location) => window.location.replace(location));
             } else {
                 response.text().then((message) => alert(message));
+                stopLoadingAnimation();
             }
         });
+    }
+
+    async function startLoadingAnimation() {
+        buttonInitialHTML = checkInButton.innerHTML;
+
+        // https://tailwindcss.com/docs/animation
+        checkInButton.innerHTML = `
+            <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+                <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Updating attendance
+        `;
+        checkInButton.classList.remove("bg-indigo-600");
+        checkInButton.classList.remove("hover:bg-indigo-700");
+        checkInButton.classList.add("bg-gray-600");
+    }
+
+    async function stopLoadingAnimation() {
+        checkInButton.innerHTML = buttonInitialHTML;
+        checkInButton.classList.remove("bg-gray-600");
+        checkInButton.classList.add("bg-indigo-600");
+        checkInButton.classList.add("hover:bg-indigo-700");
     }
 </script>
 
@@ -82,6 +109,7 @@
                 <div>
                     <button
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        bind:this={checkInButton}
                     >
                         Check in
                     </button>
