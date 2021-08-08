c6523927dcb9fca2d810c24c848f504a989e2d13
Francisco Chensan <chensanfrancisco@gmail.com>
2021-08-08 22:07:17 +0800

    Add loading animation mechanism (#75)
    * Add loading animation mechanism
    
    * Add error message box
    
    * Update error message style

--- src/routes/attendance/index.svelte
+++ src/routes/attendance/index.svelte
@@ -1,6 +1,10 @@
 <script>
     import { onMount } from 'svelte';
 
+    let buttonInitialHTML;
+    let checkInButton;
+    let errorMessage;
+
     onMount(() => {
         if (window.localStorage.getItem('name')) {
             document.querySelector('#name').value =
@@ -22,6 +26,7 @@
     });
 
     async function submitHandler() {
+        startLoadingAnimation();
         const urlParams = new URLSearchParams(window.location.search);
         const code = urlParams.get('code');
         const name = document.querySelector('#name').value;
@@ -38,9 +43,32 @@
                     .text()
                     .then((location) => window.location.replace(location));
             } else {
-                response.text().then((message) => alert(message));
+                response.text().then((message) => {errorMessage = message});
+                stopLoadingAnimation();
             }
         });
+    }
+
+    async function startLoadingAnimation() {
+        buttonInitialHTML = checkInButton.innerHTML;
+
+        // https://tailwindcss.com/docs/animation
+        checkInButton.innerHTML = `
+            <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            Updating attendance
+        `;
+        checkInButton.classList.remove("bg-indigo-600");
+        checkInButton.classList.remove("hover:bg-indigo-700");
+        checkInButton.classList.add("bg-gray-600");
+    }
+
+    async function stopLoadingAnimation() {
+        checkInButton.innerHTML = buttonInitialHTML;
+        checkInButton.classList.remove("bg-gray-600");
+        checkInButton.classList.add("bg-indigo-600");
+        checkInButton.classList.add("hover:bg-indigo-700");
     }
 </script>
 
@@ -79,9 +107,11 @@
                         class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     />
                 </div>
+                <p class:hidden={!errorMessage} class="rounded p-1.5 bg-red-500 text-xl text-white text-center">{errorMessage}</p>
                 <div>
                     <button
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        bind:this={checkInButton}
                     >
                         Check in
                     </button>
