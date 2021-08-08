902aa3c07c11a3d48a841efd378b2564d234db40
fchensan <chensanfrancisco@gmail.com>
2021-08-08 14:47:03 +0800

    Add error message box

--- src/routes/attendance/index.svelte
+++ src/routes/attendance/index.svelte
@@ -3,6 +3,7 @@
 
     let buttonInitialHTML;
     let checkInButton;
+    let errorMessage;
 
     onMount(() => {
         if (window.localStorage.getItem('name')) {
@@ -42,7 +43,7 @@
                     .text()
                     .then((location) => window.location.replace(location));
             } else {
-                response.text().then((message) => alert(message));
+                response.text().then((message) => {errorMessage = message});
                 stopLoadingAnimation();
             }
         });
@@ -53,7 +54,7 @@
 
         // https://tailwindcss.com/docs/animation
         checkInButton.innerHTML = `
-            <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24">
+            <svg class="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>
             Updating attendance
@@ -106,6 +107,7 @@
                         class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     />
                 </div>
+                <p class:hidden={!errorMessage} class="rounded-sm p-1 bg-red-500 text-xl text-white-1000 text-center">{errorMessage}</p>
                 <div>
                     <button
                         class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
